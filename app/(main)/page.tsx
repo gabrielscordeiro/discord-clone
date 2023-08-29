38193bb9c73d89889fb7bce9156f66c181d74ba6
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-08-28 22:33:32 -0300

    feat: 🎸 Authentication with Clerk.com

--- app/(main)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-       <div>
-            <p className="text-3xl font-bold text-indigo-500">
-                Discord Clone
-            </p>
-       </div>
-    )
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={openSans.className}>{children}</body>
-    </html>
+      <ClerkProvider>
+          <html lang="en">
+            <body className={openSans.className}>{children}</body>
+          </html>
+      </ClerkProvider>
   )
 }
