47fd9b7ffdbdadfcc59b13fc04a06e0a772565ae
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-08-28 21:45:08 -0300

    feat: 🎸 Enviroment setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
-  return (
-   <div>
-     <h1 className="text-3xl font-bold text-indigo-500">
-       Discord Clone
-     </h1>
-   </div>
-  )
+    return (
+       <div>
+            <p className="text-3xl font-bold text-indigo-500">
+                Discord Clone
+            </p>
+       </div>
+    )
 }
