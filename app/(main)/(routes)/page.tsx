16aa105a9b38375fb45ada672b00f05dded27198
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-08-29 21:42:59 -0300

    test: 💍 Test

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
        <div>
             <UserButton
                 afterSignOutUrl="/"
-            />
+            /> 
            <ModeToggle />
        </div>
     )
